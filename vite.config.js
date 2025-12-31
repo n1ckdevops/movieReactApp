4cda14ad72f03164ece6f9798a5542764b3624cf
n1ckdevops <nickdevops6@gmail.com>
2025-12-31 18:53:15 +0300

    fix: add bg picture

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/movieReactApp/',
   plugins: [react(), tailwindcss()],
-  envDir: './'
+  envDir: './',
+
 });
